1529b20b6f99885fb19eaaeb4da46889ff7b44a6
meltingghost <emiliodavidcc@gmail.com>
2024-09-03 11:10:20 -0400

    Revert "Merge pull request #1 from meltingghost/feature/endpoint-to-get-news"
    This reverts commit badbed9836b9b39d662ddbb7bb46c066d5e10406, reversing
    changes made to a59a345259467fbea9464101fcba3de1d54034c6.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import Footer from "./components/footer";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "../app/lib/constants";
+import Footer from "@/app/_components/footer";
+import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
-import { ThemeSwitcher } from "./components/theme-switcher";
+import { ThemeSwitcher } from "./_components/theme-switcher";
 
 import "./globals.css";
 
