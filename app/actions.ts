dd0627317f4ce298c932c969da6de9c13e00a24e
meltingghost <emiliodavidcc@gmail.com>
2024-09-04 15:54:00 -0400

    Revert "added database conection"
    This reverts commit 079e408c7ce0eead5a326864b2ab8ada7d0591cd.

--- app/actions.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-import { neon } from "@neondatabase/serverless";
-
-export async function getData() {
-  const sql = neon(process.env.DATABASE_URL || "default_connection_string");
-  const data = await sql`...`;
-  return data;
-}
