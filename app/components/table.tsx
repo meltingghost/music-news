918abcd9086f96d6a0da2952d5f345c08ca3d6d1
meltingghost <emiliodavidcc@gmail.com>
2024-09-06 14:04:54 -0400

    corrected table

--- app/components/table.tsx
+++ app/components/table.tsx
@@ -21,13 +21,13 @@ const TablePosts = async () => {
           <thead>
             <tr>
               <th className="py-2 px-4 border-b border-gray-200 bg-gray-100 text-left text-sm font-semibold text-gray-700">
-                Título
+                Title
               </th>
               <th className="py-2 px-4 border-b border-gray-200 bg-gray-100 text-left text-sm font-semibold text-gray-700">
-                Autor
+                Author
               </th>
               <th className="py-2 px-4 border-b border-gray-200 bg-gray-100 text-left text-sm font-semibold text-gray-700">
-                Descripción
+                Description
               </th>
             </tr>
           </thead>
