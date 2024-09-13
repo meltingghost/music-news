1620d744e5f3cf9e6df56278a34e5ea819627105
meltingghost <emiliodavidcc@gmail.com>
2024-09-13 16:53:27 -0400

    created Sources table

---
+++ prisma/migrations/20240913195620_add_sources_table/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - Added the required column `sourceId` to the `NewsArticle` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "NewsArticle" ADD COLUMN     "sourceId" INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Sources" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+
+    CONSTRAINT "Sources_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Sources_url_key" ON "Sources"("url");
+
+-- AddForeignKey
+ALTER TABLE "NewsArticle" ADD CONSTRAINT "NewsArticle_sourceId_fkey" FOREIGN KEY ("sourceId") REFERENCES "Sources"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
