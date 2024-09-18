1835b67a0c68c16f17532e64bf76fca9b0ab7e17
meltingghost <emiliodavidcc@gmail.com>
2024-09-18 09:58:32 -0400

    make both endpoint execute independently

---
+++ app/api/article/parse/route.ts
@@ -0,0 +1,57 @@
+import prisma from "@/lib/prisma";
+import { JSDOM } from "jsdom";
+import { Readability } from "@mozilla/readability";
+
+export async function processUnparsedArticles() {
+  try {
+    const unparsedArticles = await prisma.newsArticle.findMany({
+      where: { parsed: false },
+      select: { url: true, id: true },
+    });
+
+    for (const article of unparsedArticles) {
+      try {
+        const response = await fetch(article.url);
+        const articleHtml = await response.text();
+
+        const dom = new JSDOM(articleHtml);
+        const reader = new Readability(dom.window.document);
+        const parsedArticle = reader.parse();
+
+        if (!parsedArticle) {
+          console.error(
+            `Failed to parse article content for URL: ${article.url}`
+          );
+          await prisma.newsArticle.update({
+            where: { id: article.id },
+            data: {
+              deleted: true,
+            },
+          });
+          continue;
+        }
+
+        await prisma.newsArticle.update({
+          where: { id: article.id },
+          data: {
+            parsed: true,
+            parsedTitle: parsedArticle.title,
+            parsedContent: parsedArticle.content,
+            parsedExcerpt: parsedArticle.excerpt,
+          },
+        });
+
+        console.log(
+          `Successfully parsed and updated article with URL: ${article.url}`
+        );
+      } catch (error) {
+        console.error(
+          `Error processing article with URL: ${article.url}`,
+          error
+        );
+      }
+    }
+  } catch (error) {
+    console.error("Error fetching unparsed articles", error);
+  }
+}
---
+++ app/api/article/store/route.ts
@@ -0,0 +1,101 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function storeArticles() {
+  const token = process.env.BRAVE_API_TOKEN;
+
+  if (!token) {
+    return NextResponse.json(
+      { error: "API token is not set" },
+      { status: 500 }
+    );
+  }
+
+  try {
+    const oldestNewsSource = await prisma.sources.findFirst({
+      orderBy: {
+        updatedAt: "desc",
+      },
+    });
+
+    if (!oldestNewsSource) {
+      return NextResponse.json(
+        { error: "No sources available" },
+        { status: 500 }
+      );
+    }
+
+    await prisma.sources.update({
+      where: { id: oldestNewsSource.id },
+      data: { updatedAt: new Date() },
+    });
+
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const randomIndex = Math.floor(Math.random() * alphabet.length);
+    const keyword = alphabet[randomIndex];
+
+    const searchQuery = oldestNewsSource
+      ? `${keyword} site:${oldestNewsSource.url}`
+      : `${keyword}`;
+
+    const url =
+      "https://api.search.brave.com/res/v1/news/search?q=" + searchQuery;
+
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "X-Subscription-Token": token,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch search results");
+    }
+
+    const data = await response.json();
+
+    if (Array.isArray(data.results)) {
+      for (const article of data.results) {
+        const existingArticle = await prisma.newsArticle.findUnique({
+          where: { url: article.url },
+        });
+
+        if (!existingArticle) {
+          await prisma.newsArticle.create({
+            data: {
+              type: article.type,
+              title: article.title,
+              url: article.url,
+              description: article.description,
+              age: article.age,
+              pageAge: new Date(article.page_age),
+              scheme: article.meta_url?.scheme,
+              netloc: article.meta_url?.netloc,
+              hostname: article.meta_url?.hostname,
+              favicon: article.meta_url?.favicon,
+              path: article.meta_url?.path,
+              thumbnail: article.thumbnail?.src,
+              sourceId: oldestNewsSource.id,
+              parsed: false,
+            },
+          });
+        } else {
+          console.log(`Article with URL ${article.url} already exists.`);
+        }
+      }
+      return NextResponse.json(
+        { message: "Articles processed successfully" },
+        { status: 200 }
+      );
+    } else {
+      console.error("No results found in the API response.");
+      return NextResponse.json({ error: "No results found" }, { status: 520 });
+    }
+  } catch (error) {
+    console.error("Error fetching or storing articles:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch or store articles" },
+      { status: 500 }
+    );
+  }
+}
