91c3d863d66954c640628760d6e2aeba7bd2c697
meltingghost <emiliodavidcc@gmail.com>
2024-09-27 15:51:16 -0400

    types for the json

---
+++ prisma/types/jsonTypes.ts
@@ -0,0 +1,14 @@
+export interface TitleTranslations {
+  en: string;
+  es: string;
+}
+
+export interface ContentTranslations {
+  en: string;
+  es: string;
+}
+
+export interface ExcerptTranslations {
+  en: string;
+  es: string;
+}
