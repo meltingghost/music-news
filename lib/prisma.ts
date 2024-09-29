c3ef38794930bf2f0ec2e340d8837425463dfc58
meltingghost <emiliodavidcc@gmail.com>
2024-09-29 12:39:45 -0400

    declare json types on a global module

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,4 +2,12 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+declare global {
+  namespace PrismaJson {
+    type TitleTranslations = { en: string; es: string };
+    type ContentTranslations = { en: string; es: string };
+    type ExcerptTranslations = { en: string; es: string };
+  }
+}
+
 export default prisma;
