e4655262ea0ffefdd1d8bdf3a1d36b040619a611
meltingghost <emiliodavidcc@gmail.com>
2024-10-02 10:09:48 -0400

    added more stories

--- app/components/more-stories.tsx
+++ app/components/more-stories.tsx
@@ -1,4 +1,4 @@
-import { Post } from "@/interfaces/post";
+import Post from "@/interfaces/post";
 import { PostPreview } from "@/components/post-preview";
 
 type Props = {
@@ -11,18 +11,30 @@ export function MoreStories({ posts }: Props) {
       <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
         More Stories
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
-        {posts.map((post) => (
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="md:col-span-1">
           <PostPreview
-            key={post.slug}
-            title={post.title}
-            coverImage={post.coverImage}
-            date={post.date}
-            author={post.author}
-            slug={post.slug}
-            excerpt={post.excerpt}
+            key={posts[0].slug}
+            title={posts[0].title}
+            coverImage={posts[0].coverImage}
+            date={posts[0].publishedAt}
+            slug={posts[0].slug}
+            excerpt={posts[0].excerpt}
           />
-        ))}
+        </div>
+        <div className="flex flex-col space-y-8">
+          {posts.slice(1, 4).map((post) => (
+            <div key={post.slug}>
+              <PostPreview
+                title={post.title}
+                coverImage={post.coverImage}
+                date={post.publishedAt}
+                slug={post.slug}
+                excerpt={""}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
