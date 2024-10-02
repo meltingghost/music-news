c0ccb67b825fb093f01d2bb3ae824f865b02713a
meltingghost <emiliodavidcc@gmail.com>
2024-10-02 10:01:23 -0400

    added function to get posts, added carrousel, navbar and more posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Container from "@/app/components/container";
-import { Intro } from "@/app/components/intro";
+import { Navbar } from "@/app/components/navbar";
+import { Carrousel } from "@/app/components/carrousel";
+import { MoreStories } from "@/app/components/more-stories";
+import { getAllPosts } from "@/app/hooks/posts";
 
-export default function Index() {
+export default async function Index() {
+  const allPosts = await getAllPosts();
+  const morePosts = allPosts.slice(7);
+
   return (
     <main>
+      <Navbar />
+      <Carrousel posts={allPosts.slice(0, 5)} />
       <Container>
-        <Intro />
+        {morePosts.length > 5 && <MoreStories posts={morePosts} />}
       </Container>
     </main>
   );
