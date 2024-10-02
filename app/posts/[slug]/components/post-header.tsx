e6539ba48cbae9c673f5d7859b8fe2bcd2eec77c
meltingghost <emiliodavidcc@gmail.com>
2024-10-02 10:47:35 -0400

    i think i fixed it

--- app/posts/[slug]/components/post-header.tsx
+++ app/posts/[slug]/components/post-header.tsx
@@ -1,32 +1,32 @@
-import Avatar from "@/components/avatar";
+// import Avatar from "@/components/avatar";
 import CoverImage from "@/components/cover-image";
-import DateFormatter from "@/components/date-formatter";
+// import DateFormatter from "@/components/date-formatter";
 import { PostTitle } from "@/components/post-title";
-import { type Author } from "@/interfaces/author";
+// import { type Author } from "@/interfaces/author";
 
 type Props = {
   title: string;
   coverImage: string;
-  date: string;
-  author: Author;
+  date: Date;
+  // author: Author;
 };
 
-export function PostHeader({ title, coverImage, date, author }: Props) {
+export function PostHeader({ title, coverImage, date }: Props) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
       <div className="hidden md:block md:mb-12">
-        <Avatar name={author.name} picture={author.picture} />
+        {/* <Avatar name={author.name} picture={author.picture} /> */}
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} />
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
-          <Avatar name={author.name} picture={author.picture} />
+          {/* <Avatar name={author.name} picture={author.picture} /> */}
         </div>
         <div className="mb-6 text-lg">
-          <DateFormatter dateString={date} />
+          {/* <DateFormatter dateString={date} /> */}
         </div>
       </div>
     </>
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,41 +1,41 @@
-// import { Metadata } from "next";
-// import { notFound } from "next/navigation";
-// import { getAllPosts, getPostBySlug } from "@/app/hooks/posts";
-// import { CMS_NAME } from "@/lib/constants";
-// import markdownToHtml from "@/lib/markdownToHtml";
-// // import Alert from "@/app/posts/[slug]/components/alert";
-// import Container from "@/app/components/container";
-// import Header from "@/app/posts/[slug]/components/header";
-// import { PostBody } from "@/app/posts/[slug]/components/post-body";
-// import { PostHeader } from "@/app/posts/[slug]/components/post-header";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { getAllPosts, getPostBySlug } from "@/app/hooks/posts";
+import { CMS_NAME } from "@/lib/constants";
+import markdownToHtml from "@/lib/markdownToHtml";
+import Alert from "@/app/posts/[slug]/components/alert";
+import Container from "@/app/components/container";
+import Header from "@/app/posts/[slug]/components/header";
+import { PostBody } from "@/app/posts/[slug]/components/post-body";
+import { PostHeader } from "@/app/posts/[slug]/components/post-header";
 
-// export default async function Post({ params }: Params) {
-//   const post = getPostBySlug(params.slug);
+export default async function Post({ params }: Params) {
+  const post = getPostBySlug(params.slug);
 
-//   if (!post) {
-//     return notFound();
-//   }
+  if (!post) {
+    return notFound();
+  }
 
-//   const content = await markdownToHtml(post.content || "");
+  const content = await markdownToHtml(post.content || "");
 
-//   return (
-//     <main>
-//       {/* <Alert preview={post.preview} /> */}
-//       <Container>
-//         <Header />
-//         <article className="mb-32">
-//           {/* <PostHeader
-//             title={post.title}
-//             coverImage={post.coverImage}
-//             // date={post.date}
-//             author={post.author}
-//           /> */}
-//           <PostBody content={content} />
-//         </article>
-//       </Container>
-//     </main>
-//   );
-// }
+  return (
+    <main>
+      {/* <Alert preview={post.preview} /> */}
+      <Container>
+        <Header />
+        <article className="mb-32">
+          <PostHeader
+            title={post.title}
+            coverImage={post.coverImage}
+            date={post.publishedAt}
+            // author={post.author}
+          />
+          <PostBody content={content} />
+        </article>
+      </Container>
+    </main>
+  );
+}
 
 type Params = {
   params: {
@@ -43,30 +43,28 @@ type Params = {
   };
 };
 
-// export function generateMetadata({ params }: Params): Metadata {
-//   const post = getPostBySlug(params.slug);
+export function generateMetadata({ params }: Params): Metadata {
+  const post = getPostBySlug(params.slug);
 
-//   if (!post) {
-//     return notFound();
-//   }
+  if (!post) {
+    return notFound();
+  }
 
-//   const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
+  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
 
-//   return {
-//     title,
-//     openGraph: {
-//       title,
-//       // images: [post.ogImage.url],
-//     },
-//   };
-// }
+  return {
+    title,
+    openGraph: {
+      title,
+      images: [post.coverImage],
+    },
+  };
+}
 
-// export async function generateStaticParams() {
-//   const posts = getAllPosts();
+export async function generateStaticParams() {
+  const posts = getAllPosts();
 
-//   // return posts.map((post) => ({
-//   //   slug: post.slug,
-//   // }));
-// }
-
-export {};
+  return (await posts).map((post) => ({
+    slug: post.slug,
+  }));
+}
