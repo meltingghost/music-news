819ae46f29bd41a8f868e0195105b4745250d7f0
meltingghost <emiliodavidcc@gmail.com>
2024-10-07 17:04:27 -0400

    corrected type for locale

--- i18n/request.ts
+++ i18n/request.ts
@@ -2,8 +2,10 @@ import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
 
+type Locale = "en" | "es";
+
 export default getRequestConfig(async ({ locale }) => {
-  if (!routing.locales.includes(locale as any)) notFound();
+  if (!routing.locales.includes(locale as Locale)) notFound();
 
   return {
     messages: (await import(`@/messages/${locale}.json`)).default,
