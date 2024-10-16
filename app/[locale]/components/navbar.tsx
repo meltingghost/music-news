63de22faa2327883017a5e1a39e7db5116714f3a
meltingghost <emiliodavidcc@gmail.com>
2024-10-16 12:55:47 -0400

    use Link from i18n

--- app/[locale]/components/navbar.tsx
+++ app/[locale]/components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/link";
+import { Link } from "@/i18n/routing";
 import { useState } from "react";
 import RandomGradientLogo from "./random-gradient-logo";
 import LocalizationDropdown from "./localization-dropdown";
