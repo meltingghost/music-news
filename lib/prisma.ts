5273ef76d237554dc9a7827c5bd89cf05cd9552a
meltingghost <emiliodavidcc@gmail.com>
2024-10-16 13:00:43 -0400

    made json translations type not nullable

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,8 +4,8 @@ const prisma = new PrismaClient();
 
 declare global {
   namespace PrismaJson {
-    type TitleTranslations = { en: string | null; es: string | null };
-    type ContentTranslations = { en: string | null; es: string | null };
+    type TitleTranslations = { en: string; es: string };
+    type ContentTranslations = { en: string; es: string };
     type ExcerptTranslations = { en: string | null; es: string | null };
   }
 }
