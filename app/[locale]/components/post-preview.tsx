42e9bf7443438f36689ea3b4e01d87353fbc8ccd
meltingghost <emiliodavidcc@gmail.com>
2024-11-15 21:44:16 -0400

    make it use bold and italic

--- app/[locale]/components/post-preview.tsx
+++ app/[locale]/components/post-preview.tsx
@@ -16,12 +16,26 @@ export function PostPreview({ title, coverImage, date, excerpt, slug }: Props) {
         <div className="mb-5">
           <CoverImage title={title} src={coverImage} />
         </div>
-        <h3 className="text-3xl mb-3 leading-snug">{title}</h3>
+        <h3
+          className="text-3xl mb-3 leading-snug"
+          dangerouslySetInnerHTML={{
+            __html: title
+              .replace(/\*(.*?)\*/g, "<strong>$1</strong>")
+              .replace(/\*\*(.*?)\*\*/g, "<em>$1</em>"),
+          }}
+        />
       </Link>
       <div className="text-lg mb-4">
         <p>{date.toLocaleDateString()}</p>
       </div>
-      <p className="text-lg leading-relaxed mb-4 line-clamp-4">{excerpt}</p>
+      <p
+        className="text-lg leading-relaxed mb-4 line-clamp-4"
+        dangerouslySetInnerHTML={{
+          __html: excerpt
+            .replace(/\*(.*?)\*/g, "<strong>$1</strong>")
+            .replace(/\*\*(.*?)\*\*/g, "<em>$1</em>"),
+        }}
+      />
     </div>
   );
 }
