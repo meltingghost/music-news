f6251762897bae8e83783ad43d0a78d5713085da
meltingghost <emiliodavidcc@gmail.com>
2024-11-29 22:42:08 -0400

    add video category page

---
+++ app/[locale]/category/video/page.tsx
@@ -0,0 +1,53 @@
+import { getPostsByCategory } from "@/app/actions";
+import Carrousel from "@/app/[locale]/components/carrousel";
+import Container from "@/app/[locale]/components/container";
+import { Navbar } from "@/app/[locale]/components/navbar";
+import { MorePostsByCategory } from "@/app/[locale]/components/more-posts-by-category";
+import { VideoHeader } from "@/app/[locale]/components/headers";
+
+type Locale = "en" | "es";
+
+interface Props {
+  params: {
+    locale: Locale;
+  };
+}
+
+export default async function NewsPage({ params: { locale } }: Props) {
+  const categoryId = 6;
+  const carrouselPostsCount = 6;
+  const morePostsCount = 9;
+
+  const categoryCarrouselPosts = await getPostsByCategory(
+    categoryId,
+    0,
+    carrouselPostsCount,
+    locale
+  );
+  const categoryInitialMorePosts = await getPostsByCategory(
+    categoryId,
+    carrouselPostsCount,
+    morePostsCount,
+    locale
+  );
+
+  const carrouselPostIds = categoryCarrouselPosts.map((post) => post.id);
+
+  return (
+    <main>
+      <Navbar />
+      <VideoHeader />
+      <Carrousel posts={categoryCarrouselPosts} />
+      <Container>
+        {categoryInitialMorePosts.length > 0 && (
+          <MorePostsByCategory
+            initialPosts={categoryInitialMorePosts}
+            locale={locale}
+            excludedPostIds={carrouselPostIds}
+            categoryId={categoryId}
+          />
+        )}
+      </Container>
+    </main>
+  );
+}
