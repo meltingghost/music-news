4b463ea30049b62bda1eb58409991ea304157d7d
meltingghost <emiliodavidcc@gmail.com>
2024-11-29 22:43:08 -0400

    make search functional

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -5,38 +5,63 @@ import { PostPreview } from "../components/post-preview";
 import { useEffect, useState } from "react";
 import { getPostsBySearchResult } from "@/app/actions";
 import { Post } from "@prisma/client";
+import { notFound } from "next/navigation";
+import { Navbar } from "@/app/[locale]/components/navbar";
+import { useTranslations } from "next-intl";
 
 export default function SearchPage() {
   const searchParams = useSearchParams();
   const query = searchParams.get("query") || "";
   const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      const results = await getPostsBySearchResult(query, 0, 10, "en");
-      setPosts(results);
+      setLoading(true);
+      try {
+        const results = await getPostsBySearchResult(query, 0, 10, "en");
+        setPosts(results);
+      } finally {
+        setLoading(false);
+      }
     };
+
     if (query) fetchData();
   }, [query]);
 
+  const t = useTranslations("HomePage");
+
+  if (!posts && !loading) {
+    return notFound();
+  }
+
   return (
-    <div>
-      <h1>Search Results for "{query}"</h1>
-      {posts.length > 0 ? (
-        posts.map((post) => (
-          <div key={post.slug} className="p-4 w-full md:w-1/3 flex-grow">
-            <PostPreview
-              title={post.title}
-              coverImage={post.coverImage}
-              date={new Date(post.publishedAt)}
-              slug={post.slug}
-              excerpt={post.excerpt}
-            />
+    <main>
+      <Navbar />
+      <div className="py-11">
+        <h1 className="text-5xl mb-9 text-center">
+          {t("searchResult")}"{query}"
+        </h1>
+        {loading ? (
+          <p className="text-center text-lg">Loading...</p>
+        ) : posts.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {posts.map((post) => (
+              <div key={post.slug} className="p-4">
+                <PostPreview
+                  title={post.title}
+                  coverImage={post.coverImage}
+                  date={new Date(post.publishedAt)}
+                  slug={post.slug}
+                  excerpt={post.excerpt}
+                />
+              </div>
+            ))}
           </div>
-        ))
-      ) : (
-        <p>No results found</p>
-      )}
-    </div>
+        ) : (
+          <p className="text-center text-lg">No results found</p>
+        )}
+      </div>
+    </main>
   );
 }
