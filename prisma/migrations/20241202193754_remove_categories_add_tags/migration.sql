bf8beb55e2df228457205948c9d423c3594cec57
meltingghost <emiliodavidcc@gmail.com>
2024-12-04 06:26:22 -0400

    alter Post table, delete Category table, create Tags table

---
+++ prisma/migrations/20241202193754_remove_categories_add_tags/migration.sql
@@ -0,0 +1,44 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `categoryId` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the `Category` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_categoryId_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "categoryId";
+
+-- DropTable
+DROP TABLE "Category";
+
+-- CreateTable
+CREATE TABLE "Tags" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Tags_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_PostToTags" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tags_name_key" ON "Tags"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PostToTags_AB_unique" ON "_PostToTags"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PostToTags_B_index" ON "_PostToTags"("B");
+
+-- AddForeignKey
+ALTER TABLE "_PostToTags" ADD CONSTRAINT "_PostToTags_A_fkey" FOREIGN KEY ("A") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PostToTags" ADD CONSTRAINT "_PostToTags_B_fkey" FOREIGN KEY ("B") REFERENCES "Tags"("id") ON DELETE CASCADE ON UPDATE CASCADE;
