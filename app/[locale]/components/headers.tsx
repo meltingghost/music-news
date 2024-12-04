686c47bfc766e602f95debb0526d3b27d45721de
meltingghost <emiliodavidcc@gmail.com>
2024-12-04 06:18:34 -0400

    make tag page

--- app/[locale]/components/headers.tsx
+++ app/[locale]/components/headers.tsx
@@ -3,127 +3,21 @@
 import { useRandomGradient } from "@/app/hooks/useRandomGradient";
 import { useTranslations } from "next-intl";
 
-export function NewsHeader() {
-  const t = useTranslations("HomePage");
-  const gradient = useRandomGradient();
-
-  return (
-    <section className="w-full h-48" style={{ backgroundImage: gradient }}>
-      <div className="py-9 text-white">
-        <h1 className="text-center text-6xl text-slate-100">
-          {t("newsHeader")}
-        </h1>
-        <h2 className="text-center text-2xl text-slate-100">
-          {t("newsSubheader")}
-        </h2>
-      </div>
-    </section>
-  );
+interface TagHeaderProps {
+  tag: string | string[];
 }
 
-export function ReviewsHeader() {
+export function TagHeader({ tag }: TagHeaderProps) {
   const t = useTranslations("HomePage");
   const gradient = useRandomGradient();
 
   return (
     <section className="w-full h-48" style={{ backgroundImage: gradient }}>
-      <div className="py-9 text-white">
-        <h1 className="text-center text-6xl text-slate-100">
-          {t("reviewsHeader")}
+      <div className="py-14 text-white">
+        <h1 className="text-center text-6xl text-slate-100 capitalize">
+          {t("tagResult")}"
+          {decodeURIComponent(Array.isArray(tag) ? tag.join(" ") : tag)}"
         </h1>
-        <h2 className="text-center text-2xl text-slate-100">
-          {t("reviewsSubheader")}
-        </h2>
-      </div>
-    </section>
-  );
-}
-
-export function ReleasesHeader() {
-  const t = useTranslations("HomePage");
-  const gradient = useRandomGradient();
-
-  return (
-    <section className="w-full h-48" style={{ backgroundImage: gradient }}>
-      <div className="py-9 text-white">
-        <h1 className="text-center text-6xl text-slate-100">
-          {t("newReleasesHeader")}
-        </h1>
-        <h2 className="text-center text-2xl text-slate-100">
-          {t("newReleasesSubheader")}
-        </h2>
-      </div>
-    </section>
-  );
-}
-
-export function MiscHeader() {
-  const t = useTranslations("HomePage");
-  const gradient = useRandomGradient();
-
-  return (
-    <section className="w-full h-48" style={{ backgroundImage: gradient }}>
-      <div className="py-9 text-white">
-        <h1 className="text-center text-6xl text-slate-100">
-          {t("miscelaneousHeader")}
-        </h1>
-        <h2 className="text-center text-2xl text-slate-100">
-          {t("miscelaneousSubheader")}
-        </h2>
-      </div>
-    </section>
-  );
-}
-
-export function FeaturesHeader() {
-  const t = useTranslations("HomePage");
-  const gradient = useRandomGradient();
-
-  return (
-    <section className="w-full h-48" style={{ backgroundImage: gradient }}>
-      <div className="py-9 text-white">
-        <h1 className="text-center text-6xl text-slate-100">
-          {t("featuresHeader")}
-        </h1>
-        <h2 className="text-center text-2xl text-slate-100">
-          {t("featuresSubheader")}
-        </h2>
-      </div>
-    </section>
-  );
-}
-
-export function ListsHeader() {
-  const t = useTranslations("HomePage");
-  const gradient = useRandomGradient();
-
-  return (
-    <section className="w-full h-48" style={{ backgroundImage: gradient }}>
-      <div className="py-9 text-white">
-        <h1 className="text-center text-6xl text-slate-100">
-          {t("listsHeader")}
-        </h1>
-        <h2 className="text-center text-2xl text-slate-100">
-          {t("listsSubheader")}
-        </h2>
-      </div>
-    </section>
-  );
-}
-
-export function VideoHeader() {
-  const t = useTranslations("HomePage");
-  const gradient = useRandomGradient();
-
-  return (
-    <section className="w-full h-48" style={{ backgroundImage: gradient }}>
-      <div className="py-9 text-white">
-        <h1 className="text-center text-6xl text-slate-100">
-          {t("videoHeader")}
-        </h1>
-        <h2 className="text-center text-2xl text-slate-100">
-          {t("videoSubheader")}
-        </h2>
       </div>
     </section>
   );
---
+++ app/[locale]/tag/[tag]/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { notFound, useParams, usePathname } from "next/navigation";
+import { Navbar } from "@/app/[locale]/components/navbar";
+import { useTranslations } from "next-intl";
+import { PostPreview } from "@/app/[locale]/components/post-preview";
+import { TagHeader } from "@/app/[locale]/components/headers";
+
+type Post = {
+  id: number;
+  title: string;
+  excerpt: string;
+  slug: string;
+  coverImage: string;
+  publishedAt: string;
+};
+
+export default function TagPage() {
+  const { tag, locale } = useParams();
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (tag && locale) {
+      const fetchPosts = async () => {
+        setLoading(true);
+        try {
+          const res = await fetch(
+            `/api/tags/posts-by-tag/${tag}?locale=${locale}`
+          );
+          const data = await res.json();
+          setPosts(data);
+        } finally {
+          setLoading(false);
+        }
+      };
+      fetchPosts();
+    }
+  }, [tag, locale]);
+
+  const t = useTranslations("HomePage");
+
+  if (!posts && !loading) {
+    return notFound();
+  }
+
+  return (
+    <main>
+      <Navbar />
+      <TagHeader tag={tag} />
+      <div className="p-12">
+        {loading ? (
+          <p className="text-center text-lg">{t("loading")}</p>
+        ) : posts.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {posts.map((post) => (
+              <div key={post.id} className="p-4">
+                <PostPreview
+                  title={post.title}
+                  coverImage={post.coverImage}
+                  date={new Date(post.publishedAt)}
+                  slug={post.slug}
+                  excerpt={post.excerpt}
+                />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-center text-lg">{t("noResults")}</p>
+        )}
+      </div>
+    </main>
+  );
+}
