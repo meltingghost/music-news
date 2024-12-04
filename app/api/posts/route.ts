93729b7c7e098dac65f2a2ed8af6354a44aef964
meltingghost <emiliodavidcc@gmail.com>
2024-12-04 05:40:22 -0400

    commented because it might be useful later

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,23 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getPaginatedPostsByCategory } from "@/app/actions";
+// import { getPaginatedPostsByCategory } from "@/app/actions";
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const take = parseInt(searchParams.get("take") || "9", 10);
   const locale = searchParams.get("locale") || "en";
-  const categoryId = parseInt(searchParams.get("categoryId") || "0", 10);
+  // const categoryId = parseInt(searchParams.get("categoryId") || "0", 10);
   const loadedPostIds = searchParams.get("loadedPostIds")
     ? searchParams.get("loadedPostIds")!.split(",").map(Number)
     : [];
 
   try {
-    const posts = await getPaginatedPostsByCategory(
-      take,
-      locale as "en" | "es",
-      categoryId,
-      loadedPostIds
-    );
-    return NextResponse.json(posts);
+    // const posts = await getPaginatedPostsByCategory(
+    //   take,
+    //   locale as "en" | "es",
+    //   categoryId,
+    //   loadedPostIds
+    // );
+    // return NextResponse.json(posts);
   } catch (error) {
     console.error("Error fetching paginated posts:", error);
     return NextResponse.json(
