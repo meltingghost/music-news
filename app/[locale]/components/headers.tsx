6fa904808b4b918dfbee1e9f80d011de28ba5a33
meltingghost <emiliodavidcc@gmail.com>
2024-12-05 11:10:15 -0400

    make header for search

--- app/[locale]/components/headers.tsx
+++ app/[locale]/components/headers.tsx
@@ -7,6 +7,10 @@ interface TagHeaderProps {
   tag: string | string[];
 }
 
+interface QueryHeaderProps {
+  query: string;
+}
+
 export function TagHeader({ tag }: TagHeaderProps) {
   const t = useTranslations("HomePage");
   const gradient = useRandomGradient();
@@ -17,6 +21,21 @@ export function TagHeader({ tag }: TagHeaderProps) {
         <h1 className="text-center text-6xl text-slate-100 capitalize">
           {t("tagResult")}"
           {decodeURIComponent(Array.isArray(tag) ? tag.join(" ") : tag)}"
+        </h1>
+      </div>
+    </section>
+  );
+}
+
+export function SearchHeader({ query }: QueryHeaderProps) {
+  const t = useTranslations("HomePage");
+  const gradient = useRandomGradient();
+
+  return (
+    <section className="w-full h-48" style={{ backgroundImage: gradient }}>
+      <div className="py-14 text-white">
+        <h1 className="text-slate-100 capitalize text-5xl mb-9 text-center">
+          {t("searchResult")}"{query}"
         </h1>
       </div>
     </section>
