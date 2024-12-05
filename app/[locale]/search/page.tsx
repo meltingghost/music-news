45abd694a5485763baa2bc7a62d3ef99ccc15663
meltingghost <emiliodavidcc@gmail.com>
2024-12-05 11:09:53 -0400

    resolve locale for search results

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, usePathname } from "next/navigation";
 import { PostPreview } from "../components/post-preview";
 import { useEffect, useState } from "react";
 import { getPostsBySearchResult } from "@/app/actions";
@@ -8,6 +8,7 @@ import { Post } from "@prisma/client";
 import { notFound } from "next/navigation";
 import { Navbar } from "@/app/[locale]/components/navbar";
 import { useTranslations } from "next-intl";
+import { SearchHeader } from "@/app/[locale]/components/headers";
 
 export default function SearchPage() {
   const searchParams = useSearchParams();
@@ -15,11 +16,14 @@ export default function SearchPage() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState(true);
 
+  const pathname = usePathname();
+  const locale = pathname.split("/")[1] as "en" | "es";
+
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const results = await getPostsBySearchResult(query, 0, 10, "en");
+        const results = await getPostsBySearchResult(query, 0, 10, locale);
         setPosts(results);
       } finally {
         setLoading(false);
@@ -27,7 +31,7 @@ export default function SearchPage() {
     };
 
     if (query) fetchData();
-  }, [query]);
+  }, [query, locale]);
 
   const t = useTranslations("HomePage");
 
@@ -38,22 +42,25 @@ export default function SearchPage() {
   return (
     <main>
       <Navbar />
-      <div className="py-11">
-        <h1 className="text-5xl mb-9 text-center">
-          {t("searchResult")}"{query}"
-        </h1>
+      <SearchHeader query={query} />
+      <div className="p-9">
         {loading ? (
-          <p className="text-center text-lg">Loading...</p>
+          <div className="flex flex-col items-center justify-center">
+            <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500 mb-4"></div>
+            <p className="text-center text-lg text-blue-700 animate-pulse">
+              {t("loading")}...
+            </p>
+          </div>
         ) : posts.length > 0 ? (
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {posts.map((post) => (
               <div key={post.slug} className="p-4">
                 <PostPreview
-                  title={post.title}
+                  title={post.titleTranslations[locale]}
                   coverImage={post.coverImage}
                   date={new Date(post.publishedAt)}
                   slug={post.slug}
-                  excerpt={post.excerpt}
+                  excerpt={post.excerptTranslations[locale] || "null"}
                 />
               </div>
             ))}
